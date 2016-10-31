38abdb8af0b61444fb20fc9c6f2d39913af32de9
myungok-star <myungok.star@gmail.com>
2016-10-30 23:44:51 -0700

    added images for x and o icons

--- js/app.js
+++ js/app.js
@@ -1,27 +1,34 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-// all code to manipulate the DOM
-// goes inside this function
+    // all code to manipulate the DOM
+    // goes inside this function
 var player = true;
 
-$(".box").on("click", function(event) {
-    var selectedBox = $(this);
-    //alert the user a box that they select has already been selected.
-    if (seclectedBox.hasClass("xxx") || selectedBox.hasClass("ooo")) {
-        alert("This box has been already selected, please select a different box.")
-        // add class to selected boxes so that previously claimed boxed won't be available for selection.
-    } else {
-        if (player === true) {
-            selectedBox.addClass("xxx");
+    $(".box").on("click", function() {
+        var selectedBox = $(this);
+
+        //alert the user a box that they select has already been selected.
+        if (player) {
+                //display an image of x icon when clicked.
+            selectedBox.append('<img src="css/imgs/x.png">');
+            selectedBox.addClass("hasImg");
             player = false;
+
         } else {
-            selectedBox.addClass("ooo");
+                //display an image of o icon when clicked.
+            selectedBox.append('<img src="css/imgs/o.png">');
+            selectedBox.addClass("hasImg");
             player = true;
+
         }
+    });
+    if (selectedBox.hasClass("hasImg")) {
+      alert ("This box has already been selected, please try a different box!")
     }
-})
 
 });
+
+
 
 
 /*    function ticTacToe() {
@@ -43,12 +50,3 @@ $(".box").on("click", function(event) {
 
     ticTacToe();
     */
-
-
-
-
-
-
-
-
-});
