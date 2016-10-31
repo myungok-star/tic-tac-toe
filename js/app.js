e6b7c8e938f28a604199a924f5ee01f40b502b2e
myungok-star <myungok.star@gmail.com>
2016-10-30 22:11:41 -0700

    restarting, setting up selected box (cell) by using jquery 'this' selector

--- js/app.js
+++ js/app.js
@@ -1,38 +1,51 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  // all code to manipulate the DOM
-  // goes inside this function
-
-/*var one = $("#1");
-var two = $("#2")
-var three = $("#3")
-var four = $("#4")
-var five = $("#5")
-var six = $("#6")
-var seven = $("#7")
-var eight = $("#8")
-var nine = $("#9")*/
+// all code to manipulate the DOM
+// goes inside this function
+  var player = true;
 
 /* Testing
 $(".box").on("click", function(){
   $(this).append("it's me!");*
-});*/
+*/
+$(".box").on("click", function(event) {
+    var selectedBox = $(this);
 
-
-function ticTacToe() {
-  var player = true;
-    $(".box").on("click", function() {
-    if(player){
-      $(this).append('<img src="css/imgs/o.png">');
-      player = false;
+    if (seclectedBox.hasClass("xxx") || selectedBox.hasClass("ooo")) {
+        alert("This box has been already selected, please select another.")
     } else {
-      $(this).append('<img src="css/imgs/x.png">');
-      player = true;
+      if (player === true) {
+        selectedBox.addClass("xxx");
+        player = false;
+      } else {
+        selectedBox.addClass("ooo");
+        player = true;
       }
-    });
+    }
+})
+});
+
+
+
+/*    function ticTacToe() {
+        var player = true;
+        $(".box").on("click", function() {
+            if (player) {
+                $(this).append('<img src="css/imgs/o.png">');
+                $(this).addClass("hasImage")
+                player = false;
+            } else {
+                $(this).append('<img src="css/imgs/x.png">');
+                $(this).addClass("hasImage")
+                player = true;
+            }
+        });
     }
 
-ticTacToe();
+
+
+    ticTacToe();
+    */
 
 
 
