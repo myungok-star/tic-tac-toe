0162d032173c513c23b66245ac8e47384fd6e477
myungok-star <myungok.star@gmail.com>
2016-10-31 00:18:17 -0700

    added reset button function

--- js/app.js
+++ js/app.js
@@ -2,33 +2,41 @@
 $(document).ready(function() {
     // all code to manipulate the DOM
     // goes inside this function
-var player = true;
+    var player = true;
 
     $(".box").on("click", function() {
         var selectedBox = $(this);
 
         if (selectedBox.hasClass("hasImg")) {
-          alert ("This box has already been selected, please try a different box!")
+            alert("This box has already been selected, please try a different box!")
         }
-         
         //alert the user a box that they select has already been selected.
         else if (player) {
-                //display an image of x icon when clicked.
+            //display an image of x icon when clicked.
             selectedBox.append('<img src="css/imgs/x.png">');
             selectedBox.addClass("hasImg");
             player = false;
 
         } else {
-                //display an image of o icon when clicked.
+            //display an image of o icon when clicked.
             selectedBox.append('<img src="css/imgs/o.png">');
             selectedBox.addClass("hasImg");
             player = true;
-
         }
+
+        function resetGame() {
+            selectedBox.empty();
+            selectedBox.removeClass("hasImg");
+            var player = true;
+        }
+        $("#reset_button").on("click", function() {
+            resetGame();
+        });
+
     });
-
-
 });
+
+
 
 
 
